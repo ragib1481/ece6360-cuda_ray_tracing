cb85772c8353306985b65005b148b5bacbf9e80f
Ragib Ishrak <ragib1481@gmail.com>
2022-09-19 21:50:16 -0500

    add: report duration

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <sstream>
-#include <fstream>
-#include <string>
 #include <vector>
+#include <chrono>
 
 #include "SceneFileReader.h"
 #include "Camera.h"
@@ -10,17 +8,27 @@
 
 
 using namespace std;
+typedef std::chrono::high_resolution_clock timer;
 
 int main() {
     Camera camera(1024, 1.5);
     SceneFileReader reader("./lights.txt", "./spheres.txt");
 
+    // load data
     vector<Sphere> spheres;
     vector<Light> lights;
     spheres = reader.getSpheres();
     lights = reader.getLights();
+
+    // main render call
+    auto start = timer::now();
     camera.render(spheres, lights);
+    auto end = timer::now();
+
+    // save rendered image
     camera.saveImage("./image.tga");
 
+    std::chrono::milliseconds t = std::chrono::duration_cast<std::chrono::milliseconds> (end - start);
+    cout << "Elapsed time: " << t.count() << "ms" << endl;
     return 0;
 }
